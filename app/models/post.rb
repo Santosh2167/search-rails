8816dfcabb0492acb6606e4ce29727737283ac5b
Santosh <santosh.poudyal@gmail.com>
2018-10-24 12:04:19 +1100

    added seed to pull 1000 data from Faker gem

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
     def self.find_title_like(chars)
         where('title ILIKE ?',"%#{chars}%")
     end
+    
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do 
+1000.times do 
     Post.create(
         title: Faker::Book.title,
         content: Faker::Lorem.paragraph(2),
